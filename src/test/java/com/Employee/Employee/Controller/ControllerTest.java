d572232ddcc5f1c749a87d3d7504e5f6e87721b9
AkhilaReddyBoda <akhilareddyboda1234@gmail.com>
2021-09-06 15:25:04 +0530

    project

---
+++ src/test/java/com/Employee/Employee/Controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.Employee.Employee.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
